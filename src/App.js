16bdc2b14eb577388936e6dd1bdd6e67fcb5a053
tonenyS <gundam7784@hotmail.com>
2021-07-03 16:08:49 +0700

    tone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+// import "./App.css";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+// import tileData from './tileData';
+import image from './image.jpg';
+import Header from  "./Header.js";
+ 
+  const tileData = [
+    {
+      img: image,
+      title: 'Image',
+      author: 'author',
+      cols: 2,
+    },
+  ];
+ 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header>
+      </Header>
+      <div className={classes.root}>
+        <GridList cellHeight={160} className={classes.gridList} cols={3}>
+          {tileData.map((tile) => (
+            <GridListTile key={tile.img} cols={tile.cols || 1}>
+              <img src={tile.img} alt={tile.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
     </div>
   );
 }
---
+++ src/tileData.js
@@ -0,0 +1,12 @@
+
+import image from 'path/to/image.jpg';
+ 
+  const tileData = [
+    {
+      img: image,
+      title: 'Image',
+      author: 'author',
+      cols: 2,
+    },
+  ];
+ 
